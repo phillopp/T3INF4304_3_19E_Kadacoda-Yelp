af92c1d9fde2f40510c4c6336d193e6cda045b2b
phillopp <philip.falliano@phillopp.de>
2021-11-16 22:26:49 +0100

    Test für Assets Download

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-dvc get https://github.com/phillopp/T3INF4304_3_19E_Katacoda-Yelp t3inf4304_3_19e_yelp/assets/business.csv -o assets/business.csv
+mkdir assets
+cd assets
+wget https://github.com/phillopp/T3INF4304_3_19E_Katacoda-Yelp/raw/main/t3inf4304_3_19e_yelp/assets/business.csv
 chmod -R 777 /root/assets/
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root:/usr/src/assets abuckenhofer/columnarpostgresql:latest
 docker exec -it yelp_data psql
