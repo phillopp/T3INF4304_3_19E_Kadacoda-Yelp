e3df7375634f37ee2dc7c6e0fec41abcf957baea
phillopp <philip.falliano@phillopp.de>
2021-11-16 22:13:12 +0100

    assets

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
-
-# run in forground
-
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root/assets:/usr/src/assets abuckenhofer/columnarpostgresql:latest
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root:/usr/src/assets abuckenhofer/columnarpostgresql:latest
 docker exec -it yelp_data bash
 
-psql
 
+psql
 \i /usr/src/assets/init.sql
 \i /usr/src/assets/create-staging.sql
