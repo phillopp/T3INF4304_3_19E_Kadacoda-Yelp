45ce1d7c49835b05100cc81f683af62f0c23b403
phillopp <philip.falliano@phillopp.de>
2021-11-16 14:59:22 +0100

    Prep

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -5,14 +5,9 @@
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root/assets:/usr/src/assets abuckenhofer/columnarpostgresql:latest
 docker exec -it yelp_data bash
 
-apt-get update
-apt-get install -y python3 python3-pip
-
 psql
 
-\i /usr/src/sql/init.sql
-\i /usr/src/sql/create-staging.sql
+\i /usr/src/assets/init.sql
+\i /usr/src/assets/create-staging.sql
 
 exit;
-
-python3 /usr/src/sql/load-data.py
