dc8b47ffe90ef9ea752c9cb89cfe4a19a74e5e2a
phillopp <philip.falliano@phillopp.de>
2021-11-16 22:23:22 +0100

    load data

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
+dvc get https://github.com/phillopp/T3INF4304_3_19E_Katacoda-Yelp t3inf4304_3_19e_yelp/assets/business.csv -o assets/business.csv
+chmod -R 777 /root/assets/
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root:/usr/src/assets abuckenhofer/columnarpostgresql:latest
-docker exec -it yelp_data bash
+docker exec -it yelp_data psql
 
-
-psql
 \i /usr/src/assets/init.sql
 \i /usr/src/assets/create-staging.sql
