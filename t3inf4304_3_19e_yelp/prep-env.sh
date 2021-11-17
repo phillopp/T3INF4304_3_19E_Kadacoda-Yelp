1466642fff530e3d91b30a833a8eb969b9715cab
phillopp <philip.falliano@phillopp.de>
2021-11-17 14:01:54 +0100

    chmod

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/bash
 mkdir assets
 cd assets
-wget https://github.com/phillopp/T3INF4304_3_19E_Katacoda-Yelp/raw/main/t3inf4304_3_19e_yelp/assets/business.csv
+wget https://www.phillopp.de/dwh/business.csv
+wget https://www.phillopp.de/dwh/business_hours.csv
+wget https://www.phillopp.de/dwh/business_attributes.csv
+wget https://www.phillopp.de/dwh/business_categories.csv
+wget https://www.phillopp.de/dwh/checkin.csv
+wget https://www.phillopp.de/dwh/review.csv
+wget https://www.phillopp.de/dwh/tip.csv
+wget https://www.phillopp.de/dwh/user.csv
+wget https://www.phillopp.de/dwh/user_elite.csv
 chmod -R 777 /root/assets/
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root:/usr/src/assets abuckenhofer/columnarpostgresql:latest
-docker exec -it yelp_data psql
+
+sleep 5s
+docker exec -u root yelp_data chmod -R 777 /usr/src/assets
+
+docker exec -it yelp_data bash
+psql
 
 \i /usr/src/assets/init.sql
 \i /usr/src/assets/create-staging.sql
