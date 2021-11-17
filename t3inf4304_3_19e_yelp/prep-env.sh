10594829fde945f4ce730461ecf06ac79004a9a4
phillopp <philip.falliano@phillopp.de>
2021-11-17 15:18:41 +0100

    changed docker container

--- t3inf4304_3_19e_yelp/prep-env.sh
+++ t3inf4304_3_19e_yelp/prep-env.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/bash
-mkdir assets
-cd assets
-wget https://phillopp.de/dwh/business.csv
-wget https://phillopp.de/dwh/business_hours.csv
-wget https://phillopp.de/dwh/business_attributes.csv
-wget https://phillopp.de/dwh/business_categories.csv
-wget https://phillopp.de/dwh/checkin.csv
-wget https://phillopp.de/dwh/review.csv
-wget https://phillopp.de/dwh/tip.csv
-wget https://phillopp.de/dwh/user.csv
-wget https://phillopp.de/dwh/user_elite.csv
-chmod -R 777 /root/assets/
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data -v /root/assets:/usr/src/assets abuckenhofer/columnarpostgresql:latest
+
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name yelp_data phillopp/t3inf4303_3_19e_yelp:latest
 
 sleep 5s
-docker exec -u root yelp_data chmod -R 777 /usr/src/assets
 
 docker exec -it yelp_data bash
 psql
 
 \i /usr/src/assets/init.sql
 \i /usr/src/assets/create-staging.sql
+
+COPY staging.business FROM '/usr/src/assets/business.csv' DELIMITER ',' CSV HEADER;
+COPY staging.business_attributes FROM '/usr/src/assets/business_attributes.csv' DELIMITER ',' CSV HEADER;
+COPY staging.business_hours FROM '/usr/src/assets/business_hours.csv' DELIMITER ',' CSV HEADER;
+COPY staging.business_categories FROM '/usr/src/assets/business_categories.csv' DELIMITER ',' CSV HEADER;
+COPY staging.checkin FROM '/usr/src/assets/checkin.csv' DELIMITER ',' CSV HEADER;
+COPY staging.review FROM '/usr/src/assets/review.csv' DELIMITER ',' CSV HEADER;
+COPY staging.tip FROM '/usr/src/assets/tip.csv' DELIMITER ',' CSV HEADER;
+COPY staging.user FROM '/usr/src/assets/user.csv' DELIMITER ',' CSV HEADER;
+COPY staging.user_elite FROM '/usr/src/assets/user_elite.csv' DELIMITER ',' CSV HEADER;
